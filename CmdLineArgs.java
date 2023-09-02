a4687cc43e845588cf2265123f52725e73c985c9
V YASWANTH SRI <130354302+yaswanthsri2005@users.noreply.github.com>
2023-09-02 12:05:58 +0530

    Create CmdLineArgs.java

---
+++ CmdLineArgs.java
@@ -0,0 +1,12 @@
+// java program on cmdlinearguments
+// 02-09-2023
+// CmdLineArgs.java
+import java.io.*;
+public class CmdLineArgs {
+    public static void main(String args[]) {
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        int z = x+y;
+        System.out.println("Sum of "+x+" and "+y+" is "+z);
+    }
+}
