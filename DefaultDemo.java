8f23af26454aa05b93071c819d11434b2257f091
V YASWANTH SRI <130354302+yaswanthsri2005@users.noreply.github.com>
2023-09-09 12:35:03 +0530

    Add files via upload

---
+++ DefaultDemo.java
@@ -0,0 +1,12 @@
+import java.io.*;
+ class Sample{
+    void display(){
+        System.out.println("Hello World");
+    }
+}
+public class DefaultDemo{
+    public static void main(String args[]){
+        Sample s = new Sample();
+        s.display();
+    }
+}
