3840019638f84f307c993a6433f2491732dfd5c4
V YASWANTH SRI <130354302+yaswanthsri2005@users.noreply.github.com>
2023-09-23 11:57:49 +0530

    Create PrimeNumberV4.java

---
+++ PrimeNumberV4.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main {
+   // static {
+   //  System.out.println("Enter A Postive Integer to Check In the CommandLine Arguments");
+   // }
+  public static void main(String[] args) {
+    int num=Integer.parseInt(args[0]);
+    boolean flag = false;
+    for (int i = 2; i <= num / 2; ++i) {
+      // condition for nonprime number
+      if (num % i == 0) {
+        flag = true;
+        break;
+      }
+    }
+
+    if (!flag)
+      System.out.println(num + " is a prime number.");
+    else
+      System.out.println(num + " is not a prime number.");
+  }
+}
