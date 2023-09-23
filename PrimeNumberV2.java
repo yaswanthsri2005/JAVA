021d9f69c9decd157eaa3bec12fd638edf7b3e87
V YASWANTH SRI <130354302+yaswanthsri2005@users.noreply.github.com>
2023-09-23 11:44:08 +0530

    Add files via upload

---
+++ PrimeNumberV2.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Enter A Positive Number To Check"); 
+    Scanner sc = new Scanner(System.in);
+    int num=sc.nextInt();
+    boolean flag = false;
+    for (int i = 2; i <= num / 2; ++i) {
+      // condition for nonprime number
+      if (num % i == 0) {
+        flag = true;
+        break;
+      }
+    }
+
+    if (!flag)
+      System.out.println(num + " is a prime number.");
+    else
+      System.out.println(num + " is not a prime number.");
+  }
+}
