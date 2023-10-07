15247f79a061bb01f1a180ddbd0f5113de03eeca
V YASWANTH SRI <130354302+yaswanthsri2005@users.noreply.github.com>
2023-10-07 12:36:41 +0530

    VectorDemo.java

---
+++ VectorDemo.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class VectorDemo{
+    public static void main(String args[]){
+        Vector <String> vec = new Vector <String>();
+               vec.add("ROLEX");
+               vec.add("YASWANTH");
+               vec.add("SITE");
+               System.out.println("The Elements of the Vector are "+vec);
+    }
+}
